19276ccc40fddfde84915844643c262b8b14c470
ANTON <filimoncevvv@gmail.com>
2023-11-10 18:16:29 +0300

    Initial commit

--- Side_bar/Put_Sell.py
+++ Side_bar/Put_Sell.py
@@ -1,9 +1,11 @@
+import numpy as np
 import streamlit as st
 import pandas as pd
 import glob
 from .Support import *
 
 def put_sell():
+    st.header('Put Sell')
     # add new position
     with st.expander('Add New Put Sell Position'):
         st.write('Coming soon...')
@@ -15,5 +17,8 @@ def put_sell():
     # show all open position
     for csv_position_df in filenames:
         tick = get_tick_from_csv_name(csv_position_df)
-        st.text(tick)
-        st.dataframe(pd.read_csv(csv_position_df))
+        postion_df = pd.read_csv(csv_position_df)
+        with st.expander(tick + (' .'*20) + 'PnL: ' + postion_df['DKS'].iloc[0]):
+            st.text(tick)
+            st.dataframe(postion_df)
+
