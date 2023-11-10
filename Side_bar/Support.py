29d73ffcd95ea2423871dca725f01946b622cda5
ANTON <filimoncevvv@gmail.com>
2023-11-10 17:29:48 +0300

    Initial commit

--- Side_bar/Support.py
+++ Side_bar/Support.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def get_tick_from_csv_name(csv_position_df):
-    ticker_name = csv_position_df.split("\\")[-1].split(".")[0]
+    ticker_name = csv_position_df.split("/")[-1].split(".")[0]
     return ticker_name
