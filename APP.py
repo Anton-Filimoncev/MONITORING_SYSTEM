6ba13501338a0df3031efc1e942a48abe9c86946
ANTON <filimoncevvv@gmail.com>
2023-11-24 20:11:13 +0300

    Initial commit

--- APP.py
+++ APP.py
@@ -17,7 +17,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 infoType = st.sidebar.radio(
     "Choose an info type",
-    ('Portfolio', 'Market View', 'Put Sell', 'Call Sell', 'Strangle', 'OTM Calendar', 'ITM Calendar') # 'Call Monitoring',
+    ('Portfolio', 'Market View', 'Put Sell', 'Call Sell', 'Strangle', 'OTM Calendar', 'ITM Calendar', 'git test') # 'Call Monitoring',
 )
 
 
@@ -48,6 +48,26 @@ if infoType == 'OTM Calendar':
 # =====================================   Put Sell
 if infoType == 'ITM Calendar':
     itm_calendar()
+
+# =====================================   Put Sell
+if infoType == 'git test':
+    from git import Repo
+    PATH_OF_GIT_REPO = "https://github.com/Anton-Filimoncev/MONITORING_SYSTEM.git"
+    COMMIT_MESSAGE = 'comment from python script'
+
+
+    def git_push():
+        # try:
+        repo = Repo(search_parent_directories=True)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+        # except:
+        #     print('Some error occured while pushing the code')
+
+
+    git_push()
 
 
 
